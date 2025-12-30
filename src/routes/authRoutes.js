eba669cd872dfc8d515385bffba704dd282d37af
shahanaROHANA <kokilarubanshakana@gmail.com>
2025-12-30 11:06:25 +0530

    commend delete

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,3 @@
-// import express from 'express';
-// import { register, login, forgetPassword, resetPassword, googleAuth, googleAuthCallback } from '../controllers/authController.js';
-// import { authLimiter, passwordResetLimiter } from '../middleware/rateLimitMiddleware.js';
-// import passport from 'passport';
-
-// const router = express.Router();
-
-// // Auth routes
-// router.post('/register', authLimiter, register);
-// router.post('/login', login);
-// router.post('/forget-password', passwordResetLimiter, forgetPassword);
-// router.post('/reset-password', authLimiter, resetPassword);
-
-// // Google OAuth routes
-// router.get('/google', googleAuth);
-// router.get('/google/callback', passport.authenticate('google', { session: false }), googleAuthCallback);
 
 // export default router;
 import express from 'express';
